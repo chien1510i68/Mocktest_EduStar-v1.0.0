80583f14b0dbab679c04a5b575cafe17910bca90
VanTuan2401 <ha529160@gmail.com>
2023-11-28 09:42:14 +0700

    update28/11

--- src/router.js
+++ src/router.js
@@ -1,14 +1,40 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./page/HomePage";
 import MocktestPage from "./page/MocktestPage";
+import ExamResult from "./page/ExamResult";
+import Vstep from "./page/Vstep";
+import Toeic from "./page/Toeic";
+import BeforeExam from "./component/beforeExam/BeforeExam";
 
 export const router = createBrowserRouter([
   {
     path : "/" ,
     element : <HomePage/>
-  },{
+  },
+  
+  {
     path : "/mocktest/:examId",
     element : <MocktestPage/>
-  }
+  },
   
+  {
+    path : "/result",
+    element : <ExamResult />
+  },
+  
+  {
+    path : "/vstep",
+    element : <Vstep />
+  },
+  
+  {
+    path : "/toeic",
+    element : <Toeic />
+  },
+  
+  {
+    path : "/beforeExam",
+    element : <BeforeExam />
+  },
+
 ]);
